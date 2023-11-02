4e9e843dcd47ea70bae55e392ce6c8422d5c0d34
AxLiupore <axliupore@gmail.com>
2023-11-02 15:43:09 +0800

    repl

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sky/repl"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	content, err := os.ReadFile(filepath.Join(pwd, "banner"))
+	if err != nil {
+		panic(err)
+	}
+	sky := string(content)
+	fmt.Printf(sky)
+	repl.Start(os.Stdin, os.Stdout)
+}
